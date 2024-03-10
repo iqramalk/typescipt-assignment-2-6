4e745a1917228549ab7fb8f3bb7d9b2d6b0c4634
Iqra Asad <160169965+iqramalk@users.noreply.github.com>
2024-03-10 22:23:42 +0500

    Add files via upload
    Writing a quote using variables in different ways.

---
+++ Q5.js
@@ -0,0 +1,4 @@
+//Q5 store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. 
+var famous_person = "Quaid-e-Azam";
+var message = 'once said, "Expect the best, Prepare for the worst."';
+console.log(famous_person, message);
