0cadf588a55a767741008efd3dedb3aea19c1aa2
Iqra Asad <160169965+iqramalk@users.noreply.github.com>
2024-03-10 23:15:00 +0500

    Add files via upload
    Character combination, "\t" and "\n". Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.

---
+++ Q6.js
@@ -0,0 +1,5 @@
+//Q6 stripping names
+var my_name = "\t\n   Iqra Asad  \t\n"; //with whitespaces
+console.log(my_name);
+var stripped_name = my_name.trim();
+console.log(stripped_name); //without whitespaces
---
+++ Q6.ts
@@ -0,0 +1,8 @@
+//Q6 stripping names
+let my_name: string = "\t\n   Iqra Asad  \t\n"; 
+console.log(my_name); //with whitespaces
+
+let stripped_name:string= my_name.trim();
+
+console.log(stripped_name); //without whitespaces
+
